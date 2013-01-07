c145c94a261e93f0ebb631381ca4c13baffac1b9
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2013-01-07 15:36:57 +0100

    Prepared the workspace

---
+++ src/thor/particles.pyx
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# pyThor - Python bindings for Thor
+# Copyright 2013, Jonathan De Wachter <dewachter.jonathan@gmail.com>
+#
+# This software is released under the LGPLv3 license.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+cimport dparticles
+
+print("hello particles!")
