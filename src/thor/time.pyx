e438f736fcc852b7ee3c0bf9ae8ba8553986a297
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2013-01-08 12:35:38 +0100

    Imported sfml.system.Time

--- src/thor/time.pyx
+++ src/thor/time.pyx
@@ -8,6 +8,9 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+cimport dsystem
 cimport dtime
 
-print("hello time!")
+cdef extern from "system.h":
+	cdef class sfml.system.Time [object PyTimeObject]:
+		cdef dsystem.Time *p_this
