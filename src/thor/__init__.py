53abcd6a1c401c728b923abd2fd3ebd0f79cef6c
Jonathan De Wachter <sonkun@hesli.(none)>
2013-01-09 10:44:26 +0100

    Added __init__.py to source

---
+++ src/thor/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# pyThor - Python bindings for Thor
+# Copyright 2013, Jonathan De Wachter <dewachter.jonathan@gmail.com>
+#
+# This software is released under the LGPLv3 license.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+from thor.animation import *
+from thor.events    import *
+from thor.graphics  import *
+from thor.math      import *
+from thor.particles import *
+from thor.resources import *
+from thor.shapes    import *
+from thor.time      import *
+from thor.vectors   import *
