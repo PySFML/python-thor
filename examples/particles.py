bd7674316de2f6bfad2d340eae46ac0d9be7947d
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2013-01-22 14:15:29 +0100

    Sketched particles example

---
+++ examples/particles.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# pyThor - Python bindings for Thor
+# Copyright 2013, Jonathan De Wachter <dewachter.jonathan@gmail.com>
+#
+# This software is released under the LGPLv3 license.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+import sfml as sf
+import thor as th
+
+# create window
+window = sf.RenderWindow(sf.VideoMode(800, 600), "pyThor - Particles")
+
+# load image and initialize sprite
+try:
+	texture = sf.Texture.from_file("Media/particle.png")
+except IOError as error:
+	print(error)
+	exit(1)
+
+# create emitter
+emitter = th.UniversalEmitter()
+emitter.emission_rate = 30
+emitter.particle_lifetime = sf.seconds(5)
+emitter.particle_position = lambda: sf.Mouse.get_position(window)
+
+# create particle system
+system = th.ParticleSystem(texture)
+system.add_emitter(emitter)
+
+# build color gradient (green -> teal -> blue)
+gradient = th.createGradient(sf.Color(0, 150, 0))(1)
+                            (sf.Color(0, 150, 0))(1)
+                            (sf.Color(0, 150, 0))
+
+# create color and fade in/out animations
+colorizer = th.ColorAnimation(gradient)
+fader     = th.FadeAnimation(0.1, 0.1)
+
+# add particle affectors
+system.add_affector(th.AnimationAffector(colorizer))
+system.add_affector(th.AnimationAffector(fader))
+system.add_affector(th.TorqueAffector(100))
+system.add_affector(th.ForceAffector((0, 100)))
+
+# attributes that influence emitter
+position = sf.Vector2()
+velocity = th.PolarVector2()
+paused   = False
+
+# load font
+try:
+	font = sf.Font.from_file("Media/sansation.ttf")
+except IOError as error:
+	print(error)
+	exit(1)
+
+# instruction text
+instructions = sf.Text("Left click: Pause\n Mouse wheel: Change direction\n", font, 12)
+
+# create clock to measure frame time
+frame_clock = sf.Clock()
+
+# main loop
+while(True):
+	# handle events
+	for event in window.events:
+		# [X]: quit
+		if event == sf.CloseEvent:
+			break
+		
+		# escape: quit
+		elif event == sf.KeyEvent:
+			if event.pressed: 
+				break
+		
+		# left mouse button: enable/disable glow
+		elif event == sf.ButtonEvent:
+			if event.button == sf.Mouse.LEFT:
+				paused = not paused
+		
+		# mouse wheel: change emission direction
+		elif event == sf.WheelEvent:
+			velocity.phi += 12 * event.delta
+			
+		
+	# update particle system and emitter
+	frame_time = frame_clock.restart()
+	if not paused:
+		system.update(frame_time)
+		
+	# set initial particle velocity, rotate vector randomly by maximal 10 degrees
+	emitter.particle_velocity = th.Distributions.deflect(velocity, 10)
+	
+	# draw everything
+	window.clear(sf.Color(30, 30, 30))
+	window.draw(instructions)
+	window.draw(system)
+	window.display()
