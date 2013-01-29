68869fe23ddcea688da273b64f2f4be1c74ded3a
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2013-01-29 02:43:45 +0100

    Implemented th.Particle

--- include/dparticles.pxd
+++ include/dparticles.pxd
@@ -7,3 +7,19 @@
 # This software is released under the LGPLv3 license.
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+cimport dmath
+
+from pysfml.dsystem cimport Vector2f
+from pysfml.dgraphics cimport Color
+
+cdef extern from "Thor/Particles.hpp" namespace "thor":
+	cdef cppclass Particle:
+		Particle(Time)
+		Vector2f position
+		Vector2f velocity
+		float rotation
+		float rotationSpeed
+		Vector2f scale
+		Color color
--- src/thor/particles.pyx
+++ src/thor/particles.pyx
@@ -8,6 +8,68 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+
 cimport dparticles
 
-print("hello particles!")
+cimport pysfml.dsystem
+cimport pysfml.dgraphics
+
+from pysfml.system cimport Time, Vector2
+from pysfml.graphics cimport Color, wrap_color
+
+cdef pysfml.dsystem.Vector2f vector2_to_vector2f(vector):
+	x, y = vector
+	return pysfml.dsystem.Vector2f(x, y)
+
+cdef class Particle:
+	cdef dparticles.Particle *p_this
+
+	def __cinit__(self, Time total_lifetime):
+		self.p_this = new dparticles.Particle(total_lifetime.p_this[0])
+
+	def __dealloc__(self):
+		del self.p_this
+
+	property position:
+		def __get__(self):
+			return Vector2(self.p_this.position.x, self.p_this.position.y)
+
+		def __set__(self, position):
+			self.p_this.position = vector2_to_vector2f(position)
+
+	property velocity:
+		def __get__(self):
+			return Vector2(self.p_this.velocity.x, self.p_this.velocity.y)
+
+		def __set__(self, velocity):
+			self.p_this.velocity = vector2_to_vector2f(velocity)
+
+	property rotation:
+		def __get__(self):
+			return self.p_this.rotation
+
+		def __set__(self, float rotation):
+			self.p_this.rotation = rotation
+
+	property rotation_speed:
+		def __get__(self):
+			return self.p_this.rotationSpeed
+
+		def __set__(self, float rotation_speed):
+			self.p_this.rotationSpeed = rotation_speed
+
+	property scale:
+		def __get__(self):
+			return Vector2(self.p_this.scale.x, self.p_this.scale.y)
+
+		def __set__(self, scale):
+			self.p_this.scale = vector2_to_vector2f(scale)
+
+	property color:
+		def __get__(self):
+			cdef pysfml.dgraphics.Color* p = new pysfml.dgraphics.Color()
+			p[0] = self.p_this.color
+			return wrap_color(p)
+
+		def __set__(self, Color color):
+			self.p_this.color = color.p_this[0]
