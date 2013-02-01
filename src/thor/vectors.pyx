10d572adc5c5190dcf7ea063bc67d97b1c6da45a
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2013-02-01 03:55:14 +0100

    Dirty fix for PolarVector2 ref counter issue

--- src/thor/vectors.pyx
+++ src/thor/vectors.pyx
@@ -9,33 +9,41 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
+from libc.math cimport cos, sin
+
 cimport dvectors
 
 # TODO: replace PolarVector2Object with PolarVector2[object]
 cdef class PolarVector2:
 	cdef dvectors.PolarVector2Object *p_this
+	cdef object r, phi
 
 	def __cinit__(self, object radius, object angle):
 		self.p_this = new dvectors.PolarVector2Object(radius, angle)
+		self.r = radius
+		self.phi = angle
 
 	def __dealloc__(self):
 		del self.p_this
 
 	property r:
 		def __get__(self):
-			return self.p_this.radius()
+			return self.r
+			#return self.p_this.radius()
 
 		def __set__(self, object r):
 			self.p_this.radius(r)
-			
+			self.r = r
+
 	property phi:
 		def __get__(self):
-			return self.p_this.angle()
+			return self.phi
+			#return self.p_this.angle()
 
 		def __set__(self, object phi):
 			self.p_this.angle(phi)
-			
-	
+			self.phi = phi
+
 def length(PolarVector2 vector):
 	return dvectors.length(vector.p_this[0])
 
