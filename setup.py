b137193ea13a662895772c755f49da9d988bbe18
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2013-02-01 04:56:06 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@
 import sys, os, platform
 
 from distutils.core import setup
-from distutils.extension import Extension
+#from distutils.extension import Extension
+from Cython.Distutils import Extension
 
 # check if cython is needed (if c++ files are generated or not)
 NEED_CYTHON = not all(map(os.path.exists, [
@@ -33,19 +34,22 @@ except ValueError:
 if USE_CYTHON:
 	try:
 		from Cython.Distutils import build_ext
+		raise ImportError
+
 	except ImportError:
 		from subprocess import call
 		try:
 			if platform.system() != 'Windows':
-				call(["cython", "--cplus", "src/thor/animation.pyx", "-Iinclude"])
-				call(["cython", "--cplus", "src/thor/events.pyx", "-Iinclude"])
-				call(["cython", "--cplus", "src/thor/graphics.pyx", "-Iinclude"])
-				call(["cython", "--cplus", "src/thor/math.pyx", "-Iinclude"])
-				call(["cython", "--cplus", "src/thor/particles.pyx", "-Iinclude"])
-				call(["cython", "--cplus", "src/thor/resources.pyx", "-Iinclude"])
-				call(["cython", "--cplus", "src/thor/shapes.pyx", "-Iinclude"])
-				call(["cython", "--cplus", "src/thor/time.pyx", "-Iinclude"])
-				call(["cython", "--cplus", "src/thor/vectors.pyx", "-Iinclude"])
+				call(["cython", "--cplus", "src/thor/animation.pyx", "-Iinclude", "--fast-fail"])
+				call(["cython", "--cplus", "src/thor/events.pyx", "-Iinclude", "--fast-fail"])
+				call(["cython", "--cplus", "src/thor/graphics.pyx", "-Iinclude", "--fast-fail"])
+				call(["cython", "--cplus", "src/thor/math.pyx", "-Iinclude", "--fast-fail"])
+				#call(["cython", "--cplus", "src/thor/math/distributions.pyx", "-Iinclude", "--fast-fail"])
+				call(["cython", "--cplus", "src/thor/particles.pyx", "-Iinclude", "--fast-fail"])
+				call(["cython", "--cplus", "src/thor/resources.pyx", "-Iinclude", "--fast-fail"])
+				call(["cython", "--cplus", "src/thor/shapes.pyx", "-Iinclude", "--fast-fail"])
+				call(["cython", "--cplus", "src/thor/time.pyx", "-Iinclude", "--fast-fail"])
+				call(["cython", "--cplus", "src/thor/vectors.pyx", "-Iinclude", "--fast-fail"])
 				USE_CYTHON = False
 		except OSError:
 			print("Please install the correct version of cython and run again.")
@@ -53,33 +57,86 @@ if USE_CYTHON:
 
 # define the include directory
 if platform.system() == 'Windows':
-	sfml_include = sys.prefix + "\\include\\pysfml\\"
+	sfml_include = sys.prefix + "\\include\\pysfml"
 else:
 	major, minor, _, _ , _ = sys.version_info
-	sfml_include = sys.prefix + "/include/python{0}.{1}/sfml/".format(major, minor)
-	
+	sfml_include = sys.prefix + "/include/python{0}.{1}".format(major, minor)
+	sfml_include2 = sys.prefix + "/include/python{0}.{1}/pysfml".format(major, minor)
+
 if USE_CYTHON:
 	file_extension = "pyx"
 else:
 	file_extension = "cpp"
-	
-modules = ['animation', 'events', 'math', 'particles', 'resources', 'shapes', 'time', 'vectors']
+
+modules = ['animation', 'events', 'resources', 'shapes']
 libraries = ['sfml-system', 'sfml-window', 'sfml-graphics', 'sfml-audio', 'sfml-network']
 
 if USE_CYTHON:
 	file_extension = ".pyx"
 else:
 	file_extension = ".cpp"
-	
+
 extension = lambda name: Extension(
 	'thor.' + name,
 	sources=["src/thor/" + name + file_extension],
-	include_dirs=['include', sfml_include], 
+	include_dirs=['include', sfml_include, sfml_include2],
 	language='c++',
 	extra_compile_args = ['--std=c++0x'],
-	libraries=libraries+["thor"])
+	libraries=libraries+["thor-d"])
 
+#modules.remove("math")
 extensions = [extension(module) for module in modules]
+
+math_ext = Extension(
+	'thor.' + 'math',
+	sources=["src/thor/" + 'math' + file_extension, 'src/thor/DistributionAPI.cpp', 'src/thor/triangulation.cpp'],
+	include_dirs=['include', sfml_include, sfml_include2],
+	language='c++',
+	extra_compile_args = ['--std=c++0x', '-DNDEBUG'],
+	libraries=libraries+["thor-d"])
+
+extensions.append(math_ext)
+
+time_ext = Extension(
+	'thor.' + 'time',
+	sources=["src/thor/" + 'time' + file_extension, 'src/thor/listeners.cpp'],
+	include_dirs=['include', sfml_include, sfml_include2],
+	language='c++',
+	extra_compile_args = ['--std=c++0x'],
+	libraries=libraries+["thor-d"])
+
+extensions.append(time_ext)
+
+graphics_ext = Extension(
+	'thor.' + 'graphics',
+	sources=["src/thor/" + 'graphics' + file_extension, 'src/thor/createGradient.cpp'],
+	include_dirs=['include', sfml_include, sfml_include2],
+	language='c++',
+	extra_compile_args = ['--std=c++0x', '-fpermissive'],
+	libraries=libraries+["thor-d"])
+
+extensions.append(graphics_ext)
+
+particles_ext = Extension(
+	'thor.' + 'particles',
+	sources=["src/thor/" + 'particles' + file_extension, 'src/thor/particles/DerivableAffector.cpp', 'src/thor/particles/DerivableEmitter.cpp', 'src/thor/particles/utilities.cpp'],
+	include_dirs=['include', sfml_include, sfml_include2],
+	language='c++',
+	extra_compile_args = ['--std=c++0x'],
+	libraries=libraries+["thor-d"])
+
+extensions.append(particles_ext)
+
+vectors_ext = Extension(
+	'thor.' + 'vectors',
+	sources=["src/thor/" + 'vectors' + file_extension, 'src/thor/vectors/PolarVector2Object.cpp'],
+	include_dirs=['include', sfml_include, sfml_include2],
+	language='c++',
+	extra_compile_args = ['--std=c++0x'],
+	libraries=libraries+["thor-d"])
+
+extensions.append(vectors_ext)
+
 
 with open('README.rst', 'r') as f:
 	long_description = f.read()
